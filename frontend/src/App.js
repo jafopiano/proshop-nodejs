c553b2cd13e2b6e5d2042e20650dcebef4e3f584
Jafet <jafetoviedo12@gmail.com>
2025-01-05 01:50:45 -0600

    Instalacion de Bootrstrap y inclusion de paginas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div>Welcome To Prosho</div>
+
+    <>
+      <Header  />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome To Proshop</h1>
+        </Container>
+      </main>
+      <Footer />
+
+    </>
+    
   )
 }
 
